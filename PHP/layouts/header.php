e0b2705c66d5ca7ef916c5551d35f95f94c298de
mzaulii <89400281+mzaulii@users.noreply.github.com>
2022-09-09 12:23:53 +0200

    Add files via upload
    MZ

---
+++ PHP/layouts/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        
+
+        <!-- bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+        <!-- css -->
+        <link rel="stylesheet" href="stile.css">
+    </head>
+    <body>
+        <!-- barra di navigazione -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">MyConference</a>
+
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- ms = margin left = sposto la barra di navigazione a destra--> 
+                  <li class="nav-item"> 
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/servizi">Servizi</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/conferenze">Conferenze</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/informazioniutili">Informazioni utili</a>
+                  </li>
+                  <!--<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>-->
+                </ul>
+              </div>
+
+            </div>
+          </nav>
---
+++ PHP/login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+</head>
+<body>
+<p> 
+<?php
+   $username=$_POST["username"];
+   $password=$_POST["password"];
+   
+   // Connessione al DB
+   try {
+      $pdo=new PDO('mysql:host=localhost;dbname=Accessi','root', 'root');
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch(PDOException $e) {
+      echo("[ERRORE] Connessione al DB non riuscita. Errore: ".$e->getMessage());
+      exit();
+   }
+   
+   
+   try {
+     // Query SQL per l'inserimento dati
+     $sql='SELECT COUNT(*) AS counter FROM Login  WHERE (Utente="'.$username.'") AND (Password="'.$password.'")';
+   /*$sql='SELECT COUNT(*) AS counter FROM Login  WHERE (Utente=:lab1) AND (Password=:lab2)';
+    $res=$pdo->prepare($sql);
+    $res->bindValue(":lab1",$username);
+    $res->bindValue(":lab2",$password);
+    $res->execute();
+    */
+     //echo($sql."<br>");
+    $res=$pdo->query($sql);
+   }
+  catch(PDOException $e) {
+    echo("[ERRORE] Query SQL (Insert) non riuscita. Errore: ".$e->getMessage());
+    exit();
+  }
+  
+ 
+   $row=$res->fetch();
+   if ($row['counter']>0) {
+       echo("<b> Benvenuto nel sistema, ".$username."</b>"); 
+      } else {
+       echo("<b>Accesso non autorizzato! </b>");  
+   }
+  
+  $linkback='<br><br><a href="formlogin.html"> Torna Indietro </a>';
+  echo($linkback);
+      
+ ?>
+
+</p>
+</body>
+
+</html>
+
+<?php
+/*$sql='SELECT COUNT(*) AS counter FROM Login  WHERE (Utente=:lab1) AND (Password=:lab2)';
+     $res=$pdo->prepare($sql);
+     $res->bindValue(":lab1",$username);
+     $res->bindValue(":lab2",$password);
+     $res->execute();
+     
+     session_start();
+       $_SESSION_['user']=$username;
+     */
+?>
