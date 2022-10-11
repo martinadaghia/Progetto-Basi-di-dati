e04fda473274a8ae011100cf3920acab995975c4
mzaulii <89400281+mzaulii@users.noreply.github.com>
2022-10-11 11:29:40 +0200

    Add files via upload

---
+++ PHP/credenziali.php
@@ -0,0 +1,12 @@
+<?php
+// dati da usare per connettermi al db
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "CONFVIRTUAL";
+
+	//altro
+    $passwordAmministratore = "amministratore"; 
+    $passwordPresenter = "presenter";
+    $passwordSpeaker= "speaker";
+?>
---
+++ PHP/model/modelProfiloUtente.php
@@ -0,0 +1,29 @@
+<?php
+  require_once("../connessione.php"); // controlla se il file è già messo dentro (se è gia stato messo non lo fa)
+
+define("TITLE", "Home Page");
+include('layouts/header.php');
+?>
+
+
+<div class="position-relative overflow-hidden p-1 p-md-1 m-md-1 text-center bg-light">
+  <div class="col-md-5 p-lg-5 mx-auto my-1">
+    <h1 class="display-4 fw-normal">UTENTE</h1>
+  </div>
+  <!-- STAMPARE ELENCO CONFERENZE CON UN BOTTONE DI FIANCO PER REGISTRARSI -->
+  <!-- INSERISCI BOTTONE "PROGRAMMA GIORNALIERO" DI FIANCO AD OGNI EVENTO -->
+  <!-- PER OGNI PROGRAMMA GIORNALIERO, VISUALIZZA LE SUE SESSIONI -->
+  <!-- PER OGNI SESSIONE, VISUALIZZA LE PRESENTAZIONI -->
+
+
+
+  <!--<div class="col-md-5 mx-auto my-5">
+    <p class="lead">Visualizza l'elenco delle conferenze disponibili</p>
+    <a class="btn btn-outline-secondary" href="/info">Elenco conferenze</a>
+  </div> -->
+  </div>
+
+
+<?php
+include('layouts/footer.php');
+?>
