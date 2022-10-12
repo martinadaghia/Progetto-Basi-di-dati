74743bd5050084e2bae75f4b7b106186bead7a43
mzaulii <89400281+mzaulii@users.noreply.github.com>
2022-10-13 00:08:40 +0200

    Add files via upload

---
+++ PHP/model/layouts/footer.php
@@ -0,0 +1,18 @@
+
+        <!-- footer -->
+        <footer>
+          <div class="container pb-1 pt-1">
+            <div class="row mt-5 mb-4">
+              <div class="col-12 text-center">
+                <p>Copyright <?php echo date('Y');?> - Martina Daghia, Martina Zauli, Stefano Deri </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+        <!-- bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        
+
+    </body>
+</html>
